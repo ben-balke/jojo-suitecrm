f98fccfa2d417a529fe76c1095b27c2ba573a59a
ben-balke <ben.balke@duckdigit.com>
2026-03-30 13:00:33 -0700

    Update import of yaml by relative path.

--- bin/install-extension.php
+++ bin/install-extension.php
@@ -1,10 +1,25 @@
 <?php
 
-// bin/install-extension.php
-$targetDir = __DIR__ . '/../../../../extensions/Jojo/config/services';
-@mkdir($targetDir, 0777, true);
+declare(strict_types=1);
 
-file_put_contents(
-    $targetDir . '/jojo.yaml',
-    "imports:\n  - { resource: \"%kernel.project_dir%/vendor/ben-balke/jojo-suitecrm/config/services.yaml\" }\n"
-);
+$projectRoot = dirname(__DIR__, 4);
+$targetDir = $projectRoot . '/extensions/Jojo/config/services';
+$targetFile = $targetDir . '/jojo.yaml';
+
+if (!is_dir($targetDir) && !mkdir($targetDir, 0777, true) && !is_dir($targetDir)) {
+    fwrite(STDERR, "Failed to create directory: {$targetDir}\n");
+    exit(1);
+}
+
+$yaml = <<<YAML
+imports:
+  - { resource: "../../../../vendor/ben-balke/jojo-suitecrm/config/services.yaml" }
+
+YAML;
+
+if (file_put_contents($targetFile, $yaml) === false) {
+    fwrite(STDERR, "Failed to write file: {$targetFile}\n");
+    exit(1);
+}
+
+echo "Installed Jojo extension file: {$targetFile}\n";
